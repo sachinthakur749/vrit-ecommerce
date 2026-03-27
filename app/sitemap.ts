9b150584f224fbd260dde2f1740785d7a71a1d7a
sachin thakur <sachinthakur9983@gmail.com>
2026-03-27 11:55:53 +0545

    sitemap url fixed from vercel

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { Product } from "@/types";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = "https://vritstore.com";
+  const baseUrl = "https://vrit-ecommerce-nine.vercel.app";
   const staticUrls = [
     { url: baseUrl, lastModified: new Date() },
     { url: `${baseUrl}/products`, lastModified: new Date() },
